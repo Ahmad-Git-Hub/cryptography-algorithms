51e0af5f08721552268fab4a01ca9bd87df6bce6
Ahmad M.Naji <ahmad.mohamad.cs@gmail.com>
2024-02-16 13:27:50 +0300

    Implementing the decryption

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,27 +1,54 @@
 public class CaesarCipher {
-    private final int shift;
+    final private int shift;
+
+
+
     public CaesarCipher(int shift) {
         this.shift = shift;
     }
     public StringBuilder encrypt(String str) {
         StringBuilder cipher = new StringBuilder();
-        int t = 0;
+        int ascii;
         for(int i = 0; i < str.length(); i++) {
-            t = (int) str.charAt(i);
+            ascii = str.charAt(i);
+            if(ascii == ' ') {
+                cipher.append(' ');
+                continue;
+            }
             for(int j = 0; j < shift; j++) {
-                t++;
-                if(t > 90 && t < 97) {
-                    t = 65;
+                ascii++;
+                if(ascii > 90 && ascii < 97) {
+                    ascii = 65;
                 }
-                else if(t > 122) {
-                    t = 97;
+                else if(ascii > 122) {
+                    ascii = 97;
                 }
             }
-            cipher.append((char) t);
+            cipher.append((char) ascii);
         }
         return cipher;
     }
-    public String decrypt(String str) {
-        return "";
+    public StringBuilder decrypt(String str) {
+        StringBuilder original_text = new StringBuilder();
+        int ascii;
+        for(int i = 0; i < str.length(); i++) {
+            ascii = str.charAt(i);
+            if(ascii == ' ') {
+                original_text.append(' ');
+                continue;
+            }
+            for(int j = 0; j < shift; j++) {
+                ascii--;
+                if(ascii < 65) {
+                    ascii = 90;
+                }
+                else if(ascii > 90 && ascii < 97) {
+                    ascii = 122;
+                }
+            }
+            original_text.append((char) ascii);
+        }
+        return original_text;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    // abcdefghijklmnopqrstuvwxyz
     public static void main(String[] args) {
+
+
         CaesarCipher test = new CaesarCipher(3);
         System.out.println(test.encrypt("abc xyz ABC XYZ").toString());
-
-
+        System.out.println(test.decrypt("def abc DEF ABC").toString());
     }
 }
