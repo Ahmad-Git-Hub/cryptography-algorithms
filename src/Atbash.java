7a0ef76538d9b870c2e05d881709331eda67c43f
Ahmad M.Naji <ahmad.mohamad.cs@gmail.com>
2024-02-17 13:34:20 +0300

    Implementing the decryption method of atbash and creating user cli menu

--- src/Atbash.java
+++ src/Atbash.java
@@ -2,18 +2,36 @@ public class Atbash {
 
     public Atbash() {}
 
-    public void encrypt(String str) {
+    public String encrypt(String str) {
         StringBuilder result = new StringBuilder();
         int base;
         for(char c : str.toCharArray()) {
             if(Character.isAlphabetic(c)) {
                 base = Character.isUpperCase(c) ? 'A' : 'a';
-                result.append((char) (25 - (c - base) + base));
+                result.append((char) ((2*base) - c + 25));
             }
             else {
                 result.append(c);
             }
         }
-        System.out.println(result.toString());
+        return result.toString();
+
+    }
+
+    public String decrypt(String str) {
+        StringBuilder result = new StringBuilder();
+        int base;
+        int temp = 0;
+        for(char c : str.toCharArray()) {
+            if(Character.isAlphabetic(c)) {
+                base = Character.isUpperCase(c) ? 'A' : 'a';
+                temp = 25 - (c - base) ;
+                result.append((char) (temp+base) );
+            }
+            else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,101 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
-    // abcdefghijklmnopqrstuvwxyz
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        menu();
+        scanner.close();
+    }
 
+    public static void menu() {
+        int choice;
+        do {
+            displayMenu();
+            choice = getUserChoice();
 
-        CaesarCipher test = new CaesarCipher(3);
-        System.out.println(test.encrypt("abc xyz ABC XYZ"));
-        System.out.println(test.decrypt("def abc DEF ABC"));
+            switch (choice) {
+                case MenuOptions.CAESAR_CIPHER:
+                    handleCaesarCipher();
+                    break;
+                case MenuOptions.ATBASH_CIPHER:
+                    handleAtbashCipher();
+                    break;
+                case MenuOptions.EXIT:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != MenuOptions.EXIT);
+    }
 
-        Atbash test2 = new Atbash();
-        test2.encrypt("abc");
+    public static int getUserChoice() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a number.");
+            scanner.next();
+        }
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        return choice;
+    }
+
+    public static void handleCaesarCipher() {
+        String userInput = promptUser("encrypt");
+
+        try {
+            System.out.print("Enter number of shift: ");
+            int shift = scanner.nextInt();
+            scanner.nextLine();
+            CaesarCipher testCaesar = new CaesarCipher(shift);
+            String cipherText = testCaesar.encrypt(userInput);
+            System.out.println("Encrypted text with Caesar algorithm: " + cipherText);
+
+            userInput = promptUser("decrypt");
+            String originalText = testCaesar.decrypt(userInput);
+            System.out.println("Decrypted text with Caesar algorithm: " + originalText);
+        }
+        catch (InputMismatchException e){
+            System.out.println("A number is required");
+        }
+    }
+
+    public static void handleAtbashCipher() {
+        String userInput = promptUser("encrypt");
+        Atbash testAtbash = new Atbash();
+        String encryptionResult = testAtbash.encrypt(userInput);
+        System.out.println("Encrypted text with Atbash: " + encryptionResult);
+
+        userInput = promptUser("decrypt");
+        String decryptionResult = testAtbash.decrypt(userInput);
+        System.out.println("Decrypted text with Atbash: " + decryptionResult);
+    }
+
+    public static String promptUser(String type) {
+        if (type.equals("encrypt"))
+            System.out.print("Enter a string to encrypt: ");
+        else if (type.equals("decrypt")) {
+            System.out.print("Enter a string to decrypt: ");
+        }
+        return scanner.nextLine();
+    }
+
+    public static void displayMenu() {
+        System.out.println("╔════════════════════════════╗");
+        System.out.println("║           \033[1;33mMenu\033[0m             ║");
+        System.out.println("╠════════════════════════════╣");
+        System.out.println("║ \033[1;36m1. Caesar cipher\033[0m           ║");
+        System.out.println("║ \033[1;36m2. Atbash Cipher\033[0m           ║");
+        System.out.println("║ \033[1;36m3. Option 3\033[0m                ║");
+        System.out.println("║ \033[1;31m0. Exit\033[0m                    ║");
+        System.out.println("╚════════════════════════════╝");
+        System.out.print("\033[1mEnter your choice:\033[0m ");
+    }
+
+    static class MenuOptions {
+        public static final int CAESAR_CIPHER = 1;
+        public static final int ATBASH_CIPHER = 2;
+        public static final int EXIT = 0;
     }
 }
