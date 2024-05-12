efd2482f408fdde89918d974baa51757a4ede36f
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-05-12 19:28:54 +0300

    Fix atbash method

--- src/AtbashCipher.java
+++ src/AtbashCipher.java
@@ -1,36 +1,17 @@
+import javax.security.sasl.SaslServer;
+
 public class AtbashCipher {
-
-
-    public String encrypt(String str) {
-        StringBuilder result = new StringBuilder();
-        int base;
-        for(char c : str.toCharArray()) {
-            if(Character.isAlphabetic(c)) {
-                base = Character.isUpperCase(c) ? 'A' : 'a';
-                result.append((char) ((2*base) - c + 25));
-            }
-            else {
-                result.append(c);
+    public String encrypt(String message) {
+        String cipher = "";
+        message = message.toUpperCase();
+        for (char letter : message.toCharArray()) {
+            if (Character.isLetter(letter)) {
+                char transformed = (char) ('Z' - (letter - 'A'));
+                cipher += transformed;
+            } else {
+                cipher += letter;
             }
         }
-        return result.toString();
-
-    }
-
-    public String decrypt(String str) {
-        StringBuilder result = new StringBuilder();
-        int base;
-        int temp = 0;
-        for(char c : str.toCharArray()) {
-            if(Character.isAlphabetic(c)) {
-                base = Character.isUpperCase(c) ? 'A' : 'a';
-                temp = 25 - (c - base) ;
-                result.append((char) (temp+base) );
-            }
-            else {
-                result.append(c);
-            }
-        }
-        return result.toString();
+        return cipher;
     }
 }
